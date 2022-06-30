f938f8660994255f459020fef88f20a6c5c88027
Gene_Yang <genehcyang@gmail.com>
2022-06-29 20:08:31 -0700

    added barebones functionality of productOverview with sample data

--- client/src/components/ProductOverview/ProductInformation.jsx
+++ client/src/components/ProductOverview/ProductInformation.jsx
@@ -9,8 +9,20 @@ class ProductInformation extends React.Component {
 
   render () {
     return (
-      <div>
-      ProductInformation
+      <div id='productInformation'>
+        <div id='productInfoLeft'>
+          <b>{this.props.productData.slogan}</b>
+          <p>{this.props.productData.description}</p>
+        </div>
+        <div id='productInfoRight'>
+          <ul>
+            {this.props.productData.features.map((feature) =>
+              <li key={feature.feature}>
+                {feature.feature + ' : ' + feature.value}
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     );
   }
