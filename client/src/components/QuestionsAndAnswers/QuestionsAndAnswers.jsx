8f9688bd32892ee4db4a9fe52cc4e86e5ad5148b
asawadichai <sawadichai.andrew@gmail.com>
2022-06-29 20:21:50 -0700

    Added sample Q&A data.

--- client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchQuestions from './SearchQuestions.jsx';
 import QuestionsList from './QuestionsList.jsx';
+import questionsAndAnswers from './exampleData.js';
 
-class QuestionsAndAnswers extends React.Component{
+class QuestionsAndAnswers extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+      // eslint-disable-next-line camelcase
+      product_id: questionsAndAnswers.product_id,
+      results: questionsAndAnswers.results
+    };
   }
 
 
@@ -16,14 +20,14 @@ class QuestionsAndAnswers extends React.Component{
   }
 
   render () {
-    return(
-    <div>
-      <h1>Questions And Answers</h1>
-      <SearchQuestions/>
-      <QuestionsList/>
-      <button>More Answered Questions</button><button>Add a Question +</button>
-    </div>
-    )
+    return (
+      <div>
+        <h1>Questions And Answers</h1>
+        <SearchQuestions/>
+        <QuestionsList/>
+        <button>More Answered Questions</button><button>Add a Question +</button>
+      </div>
+    );
   }
 }
 
---
+++ client/src/components/QuestionsAndAnswers/exampleData.js
@@ -0,0 +1,67 @@
+var questionsAndAnswers = {
+  'product_id': '71697',
+  'results': [
+    {
+      'question_id': 641727,
+      'question_body': 'Would this work well for deer hunting? ',
+      'question_date': '2022-06-09T00:00:00.000Z',
+      'asker_name': 'Tyler93',
+      'question_helpfulness': 4,
+      'reported': false,
+      'answers': {
+        '5986024': {
+          'id': 5986024,
+          'body': 'I hide in the woods all the time in my camo onesie and no one has ever noticed me!',
+          'date': '2022-06-09T00:00:00.000Z',
+          'answerer_name': 'sneakyPete',
+          'helpfulness': 2,
+          'photos': []
+        },
+        '5986042': {
+          'id': 5986042,
+          'body': 'Works perfect!',
+          'date': '2022-06-11T00:00:00.000Z',
+          'answerer_name': 'jack543!',
+          'helpfulness': 0,
+          'photos': []
+        },
+        '5986043': {
+          'id': 5986043,
+          'body': 'So well!',
+          'date': '2022-06-12T00:00:00.000Z',
+          'answerer_name': 'jack543!',
+          'helpfulness': 0,
+          'photos': []
+        },
+        '5986044': {
+          'id': 5986044,
+          'body': 'so awesome!',
+          'date': '2022-06-12T00:00:00.000Z',
+          'answerer_name': 'jack543!',
+          'helpfulness': 0,
+          'photos': []
+        }
+      }
+    },
+    {
+      'question_id': 641722,
+      'question_body': 'Why did you like the product or not?',
+      'question_date': '2022-06-07T00:00:00.000Z',
+      'asker_name': 'Example: jackson11!',
+      'question_helpfulness': 2,
+      'reported': false,
+      'answers': {
+        '5986022': {
+          'id': 5986022,
+          'body': 'People can\'t see me coming in my new camo onesie!!',
+          'date': '2022-06-09T00:00:00.000Z',
+          'answerer_name': 'seakySam',
+          'helpfulness': 1,
+          'photos': []
+        }
+      }
+    }
+  ]
+};
+
+exports.questionsAndAnswers = questionsAndAnswers;
