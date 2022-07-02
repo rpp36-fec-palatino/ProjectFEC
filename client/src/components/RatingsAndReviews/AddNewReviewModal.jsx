9e35b50e6d8772c1b21a58bd976b2991b1fb3067
Danting Huang <serena.huangdanting@gmail.com>
2022-07-02 16:18:31 -0400

    radio selection for recommendation in addNewReviewModal

--- client/src/components/RatingsAndReviews/AddNewReviewModal.jsx
+++ client/src/components/RatingsAndReviews/AddNewReviewModal.jsx
@@ -5,56 +5,66 @@ class AddNewReviewModal extends React.Component {
     super(props);
     this.state = {
 
-    }
+      recommendStatus: 'yes' //default
+
+    };
   }
 
   componentDidMount () {
   }
 
 
+  onValueChange(e) {
+    this.setState({
+      recommendStatus: e.target.value
+    });
+  }
+
+
 
   render () {
     return (
-    <div>
-      <h3>Add a New Review</h3>
-      <form>
-      <div id="heading">
-        <div>Write Your Review</div>
-      </div>
-      <div name="rating">
-      {"***Start Rating component here***"}
-      </div>
-      <span id="recommend">
-        <div>Recommend: </div>
-        <button>Yes</button>
-        <button>No</button>
-
-      </span>
       <div>
-        <input type="text" name="name" placeholder="name" />
-        <br />
-        <input type="text" name="email" placeholder="email" />
-        <br />
-        <input type="text" name="summary" placeholder="summary" maxLength='60'/>
+        <h3>Add a New Review</h3>
+        <form>
+          <div id="heading">
+            <div>Write Your Review</div>
+          </div>
+          <div name="rating">
+            {'***Start Rating component here***'}
+          </div>
+          <span id="recommend">
+            <label>Recommend: </label>
+            <input type="radio" value="yes" name="recommend" checked={this.state.recommendStatus === 'yes'} onChange={e => this.onValueChange(e)} /> Yes
+            <input type="radio" value="no" name="recommend" checked={this.state.recommendStatus === 'no'} onChange={e => this.onValueChange(e)} /> No
+
+
+          </span>
+          <div>
+            <input type="text" name="name" placeholder="name" />
+            <br />
+            <input type="text" name="email" placeholder="email" />
+            <br />
+            <input type="text" name="summary" placeholder="summary" maxLength='60'/>
+          </div>
+          <div>
+            <textarea id="body" type="text" name="body" placeholder="your review..." maxLength='1000' rows="4" />
+          </div>
+
+          {'===Add Characteristic component here==='}
+          <br />
+
+          <button>Submit Review</button>
+
+
+
+
+
+        </form>
+
+
       </div>
-      <div>
-        <textarea id="body" type="text" name="body" placeholder="your review..." maxLength='1000' rows="4" />
-      </div>
-
-      {"===Add Characteristic component here==="}
-      <br />
-
-     <button>Submit Review</button>
-
-
-
-
-
-      </form>
-
-
-    </div>
-    )
+    );
   }
 }
 
