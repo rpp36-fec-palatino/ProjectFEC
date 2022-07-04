fa78286e740288a0067d62380e78c0e9db5a090c
Gene_Yang <genehcyang@gmail.com>
2022-07-04 03:30:39 -0700

    Added Size and Qty Selector, plus CSS and functionality improvements

---
+++ client/src/components/ProductOverview/AddToCart.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import css from './styles/AddToCart.module.css';
+
+class AddToCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectSize: 'default',
+      selectSizeId: '',
+      selectQty: 1
+    };
+    this.changeSize = this.changeSize.bind(this);
+    this.changeQty = this.changeQty.bind(this);
+  }
+
+  changeSize(event) {
+    let id = event.target.value;
+    this.setState({selectSize: this.props.skus[id].size, selectSizeId: id});
+  }
+
+  changeQty(event) {
+    this.setState({selectQty: event.target.value});
+  }
+
+  renderSize (skus) {
+    if (skus.null) {
+      return (<div>
+        OUT OF STOCK
+      </div>);
+    } else {
+      return (<div>
+        <select className={css.selectSize} value={this.state.selectSizeId} onChange={this.changeSize}>
+          <option value='default' key='default'>SELECT SIZE</option>
+          {Object.keys(skus).map((id) => <option value={id} key={id} label={skus[id].size}>
+            {skus[id].size}
+          </option>)}
+        </select>
+      </div>);
+    }
+  }
+
+  renderQuantity (skus) {
+    if (!skus.null) {
+      let qtyArr = [];
+      let skuQty = ((this.state.selectSizeId !== '') && (this.props.skus[this.state.selectSizeId])) ? this.props.skus[this.state.selectSizeId].quantity : 1;
+      if (skuQty > 15) {
+        skuQty = 15;
+      }
+      for (var i = 1; i <= skuQty; i++) {
+        qtyArr.push(i);
+      }
+      return (<div>
+        <select className={css.selectQty} value={this.state.selectQty} onChange={this.changeQty}>
+          {qtyArr.map((qty) => <option value={qty} key={'qty-' + qty}>{qty}</option>)}
+        </select>
+      </div>);
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <div className={css.sizeQty}>
+          {this.renderSize(this.props.skus)}
+          {this.renderQuantity(this.props.skus)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddToCart;
--- client/src/components/ProductOverview/index.jsx
+++ client/src/components/ProductOverview/index.jsx
@@ -13,8 +13,13 @@ class ProductOverview extends React.Component {
     this.state = {
       productData: exampleData.product71697,
       styleData: exampleData.productStyle71697.results,
-      selectedData: exampleData.productStyle71697.results[0]
+      selectedData: exampleData.productStyle71697.results[0],
+      currentImg: exampleData.productStyle71697.results[0].photos[0],
+      currentImgSize: 'mainImageFit',
     };
+    this.changeStyle = this.changeStyle.bind(this);
+    this.changeImage = this.changeImage.bind(this);
+    this.resize = this.resize.bind(this);
   }
 
   componentDidMount () {
@@ -31,28 +36,51 @@ class ProductOverview extends React.Component {
     } else {
       this.state.styleData.forEach((style) => {
         if (style.style_id === parseInt(styleSelect)) {
-          this.setState({selectedData: style});
+          this.setState({selectedData: style, currentImg: style.photos[0], currentImgSize: 'mainImageFit'});
         }
       });
     }
   }
 
+  changeImage (event) {
+    let selectedThumb = event.target.name;
+    if (selectedThumb !== this.state.currentImg.url) {
+      this.state.selectedData.photos.forEach((photo) => {
+        if (selectedThumb === photo.url) {
+          this.setState({currentImg: photo, currentImgSize: 'mainImageFit'});
+        }
+      });
+    }
+  }
+
+  resize (event) {
+    if (this.state.currentImgSize === 'mainImageFit') {
+      this.setState({currentImgSize: 'mainImageExpand'});
+    } else {
+      this.setState({currentImgSize: 'mainImageFit'});
+    }
+  }
+
   render () {
     return (
-      <div>
+      <div id='ProductOverview'>
         <div>Logo  _______ search bar</div>
         <div>SITE-WIDE ANNOUNCEMENT MESSAGE! - SALE/DISCOUNT OFFER - NEW PRODUCT HIGHLIGHT</div>
         <div className={css.topWindow}>
           <ImageGallery
-            photos={this.state.selectedData.photos}/>
+            photos = {this.state.selectedData.photos}
+            currentImg = {this.state.currentImg}
+            currentImgSize = {this.state.currentImgSize}
+            changeImage = {this.changeImage}
+            resize = {this.resize}/>
           <StyleSelector
-            productData={this.state.productData}
-            styleData={this.state.styleData}
-            selectedData={this.state.selectedData}
-            changeStyle={this.changeStyle.bind(this)}/>
+            productData = {this.state.productData}
+            styleData = {this.state.styleData}
+            selectedData = {this.state.selectedData}
+            changeStyle = {this.changeStyle}/>
         </div>
         <ProductInformation
-          productData={this.state.productData}/>
+          productData = {this.state.productData}/>
       </div>
     );
   }
