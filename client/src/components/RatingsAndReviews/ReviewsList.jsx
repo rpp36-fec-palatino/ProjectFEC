074763a261003121eff54f71b73b055ce7494cac
Danting Huang <serena.huangdanting@gmail.com>
2022-07-05 16:54:51 -0400

    hide coverage folder, refector ReviewsList, handle filter dropdown selection

--- client/src/components/RatingsAndReviews/ReviewsList.jsx
+++ client/src/components/RatingsAndReviews/ReviewsList.jsx
@@ -3,45 +3,111 @@ import ReviewEntry from './ReviewEntry.jsx';
 import AddNewReviewModal from './AddNewReviewModal.jsx';
 import ReviewsListCSS from './cssModule_Reviews/ReviewsList.module.css';
 
-const ReviewsList = (props) => {
-  return (
-    <div className={ReviewsListCSS.reviewListMain}>
+class ReviewsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentFilter: 'relevant' //default;
 
-      <div className="sort-slect">
-        <h2>Reviews List</h2>
-        <h3> xxx reviews, sorted by </h3>
-        <select value='relevant' >
-          <option name="relevant">Relevant</option>
-          <option name="date">Newest</option>
-          <option name="helpful">Helpful</option>
-        </select>
+    };
+
+  }
+
+  handleLoadMoreClick(e) {
+    this.props.clickLoadMoreBtn(e);
+  }
+
+  handleAddReviewClick(e) {
+    this.props.clickAddReview(e);
+  }
+
+
+
+  render() {
+    return (
+      <div className={ReviewsListCSS.reviewListMain}>
+
+        <div className="sort-slect">
+          <h2>Reviews List</h2>
+          <h3> xxx reviews, sorted by </h3>
+          <select value={this.state.currentFilter} onChange ={e => { this.setState({currentFilter: e.target.value }); }} >
+            <option value="relevant">Relevant</option>
+            <option value="newest">Newest</option>
+            <option value="helpful">Helpful</option>
+          </select>
+        </div>
+        <br/>
+        {'--------------------------------------------------------------'}
+
+        <div className={ReviewsListCSS.reviewScroller} >
+          {this.props.currentDisplayReviews.map(
+            review => <ReviewEntry key = {review.review_id} review = {review} />
+          )}
+        </div>
+
+        {/* conditional rendering when there are more than 2 reviews */}
+        {this.props.currentReviews.length > 2 && this.props.loadMore
+          ? <button onClick={this.handleLoadMoreClick.bind(this)}>MORE REVIEWS</button>
+          : null
+        }
+
+        <button onClick={this.handleAddReviewClick.bind(this)}>ADD A NEW REVIEW +</button>
+
+        {this.props.addReview ? <AddNewReviewModal /> : null}
+
+
+
+
+
       </div>
-      <br/>
-      {'--------------------------------------------------------------'}
-
-      <div className={ReviewsListCSS.reviewScroller} >
-        {props.currentDisplayReviews.map(
-          review => <ReviewEntry key = {review.review_id} review = {review} />
-        )}
-      </div>
-
-      {/* conditional rendering when there are more than 2 reviews */}
-      {props.currentReviews.length > 2 && props.loadMore
-        ? <button onClick={e => props.clickLoadMoreBtn(e)}>MORE REVIEWS</button>
-        : null
-      }
-
-      <button onClick={e => props.clickAddReview(e)}>ADD A NEW REVIEW +</button>
-
-      {props.addReview ? <AddNewReviewModal /> : null}
+    );
+  }
 
 
 
+}
 
 
-    </div>
-  );
-};
+
+// const ReviewsList = (props) => {
+// return (
+//   <div className={ReviewsListCSS.reviewListMain}>
+
+//     <div className="sort-slect">
+//       <h2>Reviews List</h2>
+//       <h3> xxx reviews, sorted by </h3>
+//       <select value='relevant' onChange ={e => {}} >
+//         <option name="relevant">Relevant</option>
+//         <option name="date">Newest</option>
+//         <option name="helpful">Helpful</option>
+//       </select>
+//     </div>
+//     <br/>
+//     {'--------------------------------------------------------------'}
+
+//     <div className={ReviewsListCSS.reviewScroller} >
+//       {props.currentDisplayReviews.map(
+//         review => <ReviewEntry key = {review.review_id} review = {review} />
+//       )}
+//     </div>
+
+//     {/* conditional rendering when there are more than 2 reviews */}
+//     {props.currentReviews.length > 2 && props.loadMore
+//       ? <button onClick={e => props.clickLoadMoreBtn(e)}>MORE REVIEWS</button>
+//       : null
+//     }
+
+//     <button onClick={e => props.clickAddReview(e)}>ADD A NEW REVIEW +</button>
+
+//     {props.addReview ? <AddNewReviewModal /> : null}
+
+
+
+
+
+//   </div>
+// );
+// };
 
 
 
