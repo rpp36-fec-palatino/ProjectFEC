3337eb44930c16e5e1bc430bcf927ad43acc1d53
Danting Huang <serena.huangdanting@gmail.com>
2022-07-05 02:16:32 -0400

    minor change in test file. use screen.getByText

--- client/src/components/RatingsAndReviews/RatingAndReviewsSpec/RatingsAndReviews.test.js
+++ client/src/components/RatingsAndReviews/RatingAndReviewsSpec/RatingsAndReviews.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import RatingsAndReviews from '../RatingsAndReviews.jsx';
 import ReviewsList from '../ReviewsList.jsx';
@@ -12,16 +12,16 @@ import {sampleReviews71698} from '../../../../../sampleData/sampleReviewData.js'
 // use describe, it pattern
 describe('<RatingsAndReviews /> and its subcomponents rendering', () => {
   it('Renders <RatingsAndReviews /> component correctly', () => {
-    const { getByText } = render(<RatingsAndReviews />);
-    expect(getByText(/Ratings and Reviews/i)).toBeInTheDocument();
+    render(<RatingsAndReviews />);
+    expect(screen.getByText(/Ratings and Reviews/i)).toBeInTheDocument();
   });
   it('Renders <ReviewsList /> component correctly', () => {
-    const { getByText } = render(<ReviewsList
+    render(<ReviewsList
       currentReviews = {sampleReviews71698.results}
       currentDisplayReviews = {sampleReviews71698.results.slice(0, 2)}
 
     />);
-    expect(getByText(/Reviews List/i)).toBeInTheDocument();
+    expect(screen.getByText(/Reviews List/i)).toBeInTheDocument();
   });
 
 });
