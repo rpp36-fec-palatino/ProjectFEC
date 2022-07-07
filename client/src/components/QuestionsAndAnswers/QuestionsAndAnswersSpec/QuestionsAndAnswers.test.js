1002550f97ec98db4b32a27b08845057635859aa
asawadichai <sawadichai.andrew@gmail.com>
2022-07-06 18:40:11 -0700

    Added jest testing of components

---
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswersSpec/QuestionsAndAnswers.test.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import QuestionsAndAnswers from '../QuestionsAndAnswers.jsx';
+import QuestionsList from '../QuestionsList.jsx';
+
+describe('questions and answers module and subcomponent rendering', () => {
+  it('Renders QuestionsAndAnswers component', () => {
+    render(<QuestionsAndAnswers />);
+    expect(screen.getByText(/Questions and Answers/i)).toBeInTheDocument();
+  });
+});
