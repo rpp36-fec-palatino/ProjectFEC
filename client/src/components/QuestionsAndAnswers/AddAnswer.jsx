cfffbaa17f2cc5fde96157b33a106f22ae7fa52c
asawadichai <sawadichai.andrew@gmail.com>
2022-07-29 10:14:05 -0700

    WIP: photo upload to image hosting.

--- client/src/components/QuestionsAndAnswers/AddAnswer.jsx
+++ client/src/components/QuestionsAndAnswers/AddAnswer.jsx
@@ -7,32 +7,36 @@ import Wrapper from '../../Wrapper.jsx';
 
 const AddAnswer = (props) => {
   const [images, setImages] = useState([]);
+  const [imageFiles, setImageFile] = useState([]);
   const url = '/qa/questions/answers';
 
   return (
     <div id="answerModal" className={style.modal}>
-      <h4>Submit your answer for {props.productName}: {props.questionBody}</h4>
-      <form>
+      <h4 id="addAnswerTitle">Submit your answer for {props.productName}: {props.questionBody}</h4>
+      <form id="addAnswerForm">
         <input type="hidden" id="question_id" name="question_id" value={props.questionId}></input>
-        <label for="yourAnswer"><b>Your Answer*</b></label><br></br>
-        <textarea name="body" id="body" rows="10" cols="30" maxLength="1000" required></textarea><br></br>
-        <label for="upload"><b>Upload your photos (limit 5)</b></label><br></br>
+        <label for="yourAnswer"><b id="addAnswerBodyTitle">Your Answer*</b></label><br></br>
+        <textarea name="body" id="addAnswerBody" rows="10" cols="30" maxLength="1000" required></textarea><br></br>
+        <label for="upload"><b id="addAnswerPhotoUploadLimitText">Upload your photos (limit 5)</b></label><br></br>
         {images.length ? <div>{ images.map((photo) => (
           <img className={style.thumbnail} src={photo}></img>
-        ) ) }</div> : <p>No photos yet</p>}
-        {(images.length < 5) ? <input type="file" name="upload" onChange={(e) => {
+        ) ) }</div> : <p id="addAnswerNoPhotoText">No photos yet</p>}
+        {(images.length < 5) ? <input type="file" name="upload" id="addAnswerUploadButton" onChange={(e) => {
           if (e.target.files && e.target.files[0]) {
+            console.log('image upload', e.target.files);
             let img = e.target.files[0];
             let photos = images;
+            let files = imageFiles;
+
             photos.push(URL.createObjectURL(img));
             setImages(photos);
           }
         }}></input> : null}
-        <br></br><label for="name"><b>What is your nickname*</b></label><br></br>
+        <br></br><label for="name"><b id="addAnswerNicknameTitle">What is your nickname*</b></label><br></br>
         <input type="text" id="name" name="name" placeholder="Example: jack543!" maxLength="60" required></input><br></br>
         <label for="email"><b>Your email*</b></label><br></br>
         <input type="email" id="email" name="email" placeholder="Example: jack@email.com" maxLength="60" required></input><br></br>
-        <label for="email">For authentication reasons, you will not be emailed</label><br></br>
+        <label for="email" id="addAnswerAuthenticationText">For authentication reasons, you will not be emailed</label><br></br>
         <input type="button" id="addAnswerSubmit" value="Submit" onClick={() => {
           var options = {
             // eslint-disable-next-line camelcase
